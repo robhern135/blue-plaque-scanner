d3d9e5d883568db459cbd0767cdfecdc94507802
Rob Hern <robertjhern@gmail.com>
2025-05-21 08:55:47 +0100

    change key

--- helperFunctions.js
+++ helperFunctions.js
@@ -1,5 +1,5 @@
 //file name: helperFunctions.js
-const API_KEY = "AIzaSyBZnsNGvXdgQFl_b3oHNu_6lhp6igwPcWE" //put your key here.
+const API_KEY = "" //put your key here.
 //this endpoint will tell Google to use the Vision API. We are passing in our key as well.
 const API_URL = `https://vision.googleapis.com/v1/images:annotate?key=${API_KEY}`
 function generateBody(image) {
